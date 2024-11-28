2d1863bb3054f6c1de30d64f2403c6b50fda35c8
akram <akram10shaikh@gmail.com>
2024-11-28 21:20:11 +0530

    commit

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install --upgrade pip
+pip install gunicorn
+pip install git+https://github.com/benoitc/gunicorn.git
+pip install -U git+https://github.com/benoitc/gunicorn.git
+pip install greenlet
+pip install eventlet
+pip install gunicorn[eventlet]
+pip install gevent
+pip install gunicorn[gevent]
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+
+python manage.py migrate
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+fi
